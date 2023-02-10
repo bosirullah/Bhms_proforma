c059d3631700ad1c421b9f05c6fb42ab3686b5b6
MD SAHI ALAM KHAN <ksahi4088@gmail.com>
2023-02-11 00:37:01 +0530

    Login page added

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.set("view engine","ejs");
 app.use(express.static("public"));
 
 app.get("/",(req,res)=>{
-    res.render("page1");
+    res.render("login");
 });
 
 app.get("/:customRouteName",(req,res)=>{
